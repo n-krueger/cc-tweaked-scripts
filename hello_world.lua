18638bc4f517538e0c4aaa9ccda2db411df154ae
Vincent Jeltsch <jeltsch.vincent@gmail.com>
2023-03-31 22:50:42 +0200

    adds mob counting
    Co-authored-by: Nils Krüger <n-krueger@users.noreply.github.com>

--- hello_world.lua
+++ hello_world.lua
@@ -1,1 +1,7 @@
-print("Hello Computercraft!")
+print("Let's detect some mobs!")
+
+local sensor = peripheral.wrap("right")
+local entities = sensor.sense()
+local n = #entities
+
+print("# mobs detected: " + n)
