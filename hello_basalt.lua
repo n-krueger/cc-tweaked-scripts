85e682bf4ec3139dad0712150b800e019d07d869
Vincent Jeltsch <jeltsch.vincent@gmail.com>
2023-03-31 23:48:56 +0200

    adds click counting

--- hello_basalt.lua
+++ hello_basalt.lua
@@ -2,13 +2,16 @@ local basalt = require("basalt")
 
 local main = basalt.createFrame()
 
+local clickCount = 0
+
 local button = main:addButton()
 button:setPosition(4, 4)
 button:setSize(16, 3)
 button:setText("Click me!")
 
 local function buttonClick()
-    basalt.debug("I got clicked!")
+    clickCount = clickCount + 1
+    basalt.debug("# of button clicks: " .. clickCount)
 end
 
 button:onClick(buttonClick)
