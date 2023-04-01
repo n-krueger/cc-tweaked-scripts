5c7e131fb8ec25db68d2bb60f5506a71b3bcd597
Nils Krüger <36828049+n-krueger@users.noreply.github.com>
2023-04-01 14:57:02 +0200

    add prints
    Co-authored-by: Vincent Jeltsch <jeltsch.vincent@gmail.com>

--- mob_spawn.lua
+++ mob_spawn.lua
@@ -24,17 +24,19 @@ while true do
     local color_mask = 0
 
     for mob, count in pairs(mob_counts_remaining) do
+        print("Spawning  " .. mob)
         if count > 0 then
             color_mask = colors.combine(color_mask, mob_colors[mob])
             mob_counts_remaining[mob] = count - 1
-        end        
+        end
     end
 
     if color_mask == 0 then
+        print("No mobs left to spawn")
         break
     end
 
     redstone.setBundledOutput(redstone_face, color_mask)
-    sleep(0.1)
+    sleep(0.5)
     redstone.setBundledOutput(redstone_face, 0)
 end
