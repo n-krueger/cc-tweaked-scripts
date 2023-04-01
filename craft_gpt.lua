6343c24da16518fefc7ad90ee12597821c7f7d3f
Vincent Jeltsch <jeltsch.vincent@gmail.com>
2023-04-01 17:43:32 +0200

    adds craft gpt script

---
+++ craft_gpt.lua
@@ -0,0 +1,29 @@
+local io = require('io')
+local textutils = require('textutils')
+local http = require('http')
+
+local api_key
+while api_key == "" do
+    io.write("Please enter ChatGPT API key:")
+    io.flush()
+    api_key = io.read("l")
+end
+
+local url = "https://api.openai.com/v1/chat/completions"
+local body = {
+    model = "gpt-3.5-turbo",
+    messages = {
+        {
+            role = "user",
+            content = "Your name is now Trusty. You are an advanced AI inside of a Minecraft world supporting the players with your vast knowledge."
+        }
+    }
+}
+local body_json = textutils.serializeJSON(body)
+local headers = {
+    Authorization = "Bearer " .. api_key 
+}
+
+local request = http.post(url, body_json, headers)
+print(request.readAll())
+request.close()
