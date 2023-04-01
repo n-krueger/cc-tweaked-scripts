d883b7397ab2bddbc2c8fce4e19b8e2b0325f073
Nils Krüger <36828049+n-krueger@users.noreply.github.com>
2023-04-02 01:24:01 +0200

    add counts per item type

--- farmmon/farm.lua
+++ farmmon/farm.lua
@@ -32,12 +32,27 @@ function Farm:_iter_slots(slots)
     )
 end
 
+function Farm:_item_iter_counts(it)
+    return fun.reduce(
+        function(acc, item_meta)
+            acc[item_meta.name] = (acc[item_meta.name] or 0) + item_meta.count
+            return acc
+        end,
+        {},
+        it
+    )
+end
+
 function Farm:iter_soil()
     return self:_iter_slots(soil_slots())
 end
 
 function Farm:list_soil()
     return fun.totable(self:iter_soil())
+end
+
+function Farm:get_soil_counts()
+    return self:_item_iter_counts(self:iter_soil())
 end
 
 function Farm:iter_seed()
@@ -48,6 +63,10 @@ function Farm:list_seed()
     return fun.totable(self:iter_seed())
 end
 
+function Farm:get_seed_counts()
+    return self:_item_iter_counts(self:iter_seed())
+end
+
 function Farm:iter_output()
     return self:_iter_slots(output_slots())
 end
@@ -56,8 +75,16 @@ function Farm:list_output()
     return fun.totable(self:iter_output())
 end
 
+function Farm:get_output_counts()
+    return self:_item_iter_counts(self:iter_output())
+end
+
 function Farm:get_fertilizer()
     return self.peripheral.getItemMeta(fertilizer_slot)
+end
+
+function Farm:get_fertilizer_count()
+    return self:get_fertilizer().count
 end
 
 return Farm
