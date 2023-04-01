7eaf6b45c721b298d429b141760033af0f8bca6d
Nils Krüger <36828049+n-krueger@users.noreply.github.com>
2023-04-02 00:05:10 +0200

    print farm directly

--- farmmon/main.lua
+++ farmmon/main.lua
@@ -9,4 +9,4 @@ local farm_spruce_2 = peripheral.wrap("forestry:farm_5")
 local farm_spruce_3 = peripheral.wrap("forestry:farm_6")
 local farm_spruce_4 = peripheral.wrap("forestry:farm_7")
 
-pretty.print(pretty.pretty(farm_ender_1.list()))
+pretty.print(pretty.pretty(farm_ender_1))
