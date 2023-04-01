4d29437643be7d230ee42c9bb4b191ebd8a95d3c
Nils Krüger <36828049+n-krueger@users.noreply.github.com>
2023-04-01 14:26:55 +0200

    disable black redstone output

--- hello_redstone.lua
+++ hello_redstone.lua
@@ -1,6 +1,6 @@
 print("Enabling Lamps")
 
-local colorMask = colors.combine(colors.black, colors.white)
+local colorMask = colors.combine(colors.white)
 redstone.setBundledOutput("front", colorMask)
 sleep(0.5)
 redstone.setBundledOutput("front", 0)
