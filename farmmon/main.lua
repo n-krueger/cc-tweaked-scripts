a692890d86ef12f26945f0036839e0ad392520b6
Nils Krüger <36828049+n-krueger@users.noreply.github.com>
2023-04-01 23:56:31 +0200

    wrap farm peripherals

--- farmmon/main.lua
+++ farmmon/main.lua
@@ -1,11 +1,12 @@
 local fun = require("fun")
+local pretty = require("cc.pretty")
 
-local n = 100
-local test = fun.sum(
-    fun.map(
-        function (x) return x^2 end,
-        fun.take(n, fun.tabulate(math.sin))
-    )
-)
+local farm_ender_1 = peripheral.wrap("forestry:farm_1")
+local farm_barley_1 = peripheral.wrap("forestry:farm_2")
+local farm_wheat_1 = peripheral.wrap("forestry:farm_3")
+local farm_spruce_1 = peripheral.wrap("forestry:farm_4")
+local farm_spruce_2 = peripheral.wrap("forestry:farm_5")
+local farm_spruce_3 = peripheral.wrap("forestry:farm_6")
+local farm_spruce_4 = peripheral.wrap("forestry:farm_7")
 
-print(test)
+pretty.print(pretty.pretty(farm_ender_1))
