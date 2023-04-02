d43583308a963261e7f7cbeb384cbc4783174095
Nils Krüger <36828049+n-krueger@users.noreply.github.com>
2023-04-02 15:48:44 +0200

    remove unnecessary bg fg specifiers

--- farmmon/main.lua
+++ farmmon/main.lua
@@ -149,11 +149,7 @@ local function calculate_farm_aggregates()
     return farm_aggregates
 end
 
-local main_frame = basalt.createFrame()
-    :setMonitor("right", 0.5)
-    :setBackground(colors.black)
-    :setForeground(colors.white)
-    :setBorder(colors.black)
+local main_frame = basalt.createFrame():setMonitor("right", 0.5)
 
 local farm_update_thread = main_frame:addThread()
 farm_update_thread:start(function()
@@ -194,9 +190,6 @@ local farm_frames = fun.iter(farms)
             :addFrame(frame_id)
             :setSize(width, height)
             :setPosition(pos_x, pos_y)
-            :setBackground(colors.black)
-            :setForeground(colors.white)
-            :setBorder(colors.black)
             :addLayout(fs.combine(base_dir, "farm_frame.xml"))
         
         local title_label = sub_frame:getObject("label.title")
