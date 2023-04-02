21fe85bf5010ec4722700689046d67a65dd4447e
Nils Krüger <36828049+n-krueger@users.noreply.github.com>
2023-04-02 03:51:51 +0200

    add aggregation

--- farmmon/main.lua
+++ farmmon/main.lua
@@ -23,6 +23,24 @@ local farm_diffs = fun.tomap(fun.map(
     fun.iter(farms)
 ))
 
+local function sum_tables(a, b)
+    local res = {}
+
+    for k, v in pairs(a) do
+        if v >= 0 then
+            res[k] = v
+        end
+    end
+
+    for k, v in pairs(b) do
+        if v >= 0 then
+            res[k] = (res[k] or 0) + v
+        end
+    end
+
+    return res
+end
+
 local function diff_tables(a, b)
     return fun.tomap(fun.map(
         function(k, v) return k, v - (a[k] or 0) end,
@@ -32,44 +50,71 @@ end
 
 local runtime = 20
 local n_iters = runtime * 20
-print("Collecting data for " .. runtime .. "s")
 
-for i=1,n_iters do
-    local funcs = fun.totable(fun.map(
-        function(key, farm)
-            return function()
-                farm:fetch()
-                
-                local res = {
-                    soil_counts = farm:get_soil_counts(),
-                    seed_counts = farm:get_seed_counts(),
-                    output_counts = farm:get_output_counts(),
-                    fertilizer_count = farm:get_fertilizer_count(),
-                }
-                farm_counts[key][i] = res
+while true do
+    print("Collecting data for " .. runtime .. "s...")
 
-                local prev = farm_counts[key][i-1]
-                local diff = prev == nil
-                    and {
-                        soil_counts = diff_tables(res.soil_counts, res.soil_counts),
-                        seed_counts = diff_tables(res.seed_counts, res.seed_counts),
-                        output_counts = diff_tables(res.output_counts, res.output_counts),
-                        fertilizer_count = 0,
+    for i=1,n_iters do
+        local funcs = fun.totable(fun.map(
+            function(key, farm)
+                return function()
+                    farm:fetch()
+                    
+                    local res = {
+                        soil_counts = farm:get_soil_counts(),
+                        seed_counts = farm:get_seed_counts(),
+                        output_counts = farm:get_output_counts(),
+                        fertilizer_count = farm:get_fertilizer_count(),
                     }
-                    or {
-                        soil_counts = diff_tables(prev.soil_counts, res.soil_counts),
-                        seed_counts = diff_tables(prev.seed_counts, res.seed_counts),
-                        output_counts = diff_tables(prev.output_counts, res.output_counts),
-                        fertilizer_count = res.fertilizer_count - prev.fertilizer_count,
+                    farm_counts[key][i] = res
+    
+                    local prev = farm_counts[key][i-1]
+                    local diff = prev == nil
+                        and {
+                            soil_counts = diff_tables(res.soil_counts, res.soil_counts),
+                            seed_counts = diff_tables(res.seed_counts, res.seed_counts),
+                            output_counts = diff_tables(res.output_counts, res.output_counts),
+                            fertilizer_count = 0,
+                        }
+                        or {
+                            soil_counts = diff_tables(prev.soil_counts, res.soil_counts),
+                            seed_counts = diff_tables(prev.seed_counts, res.seed_counts),
+                            output_counts = diff_tables(prev.output_counts, res.output_counts),
+                            fertilizer_count = res.fertilizer_count - prev.fertilizer_count,
+                        }
+                    farm_diffs[key][i] = diff
+                end
+            end,
+            fun.iter(farms)
+        ))
+    
+        parallel.waitForAll(table.unpack(funcs))
+    end
+
+    local farm_aggregates = fun.tomap(fun.map(
+        function(key, _)
+            return fun.reduce(
+                function(acc, x)
+                    local res = {
+                        soil_counts = sum_tables(acc.soil_counts, x.soil_counts),
+                        seed_counts = sum_tables(acc.seed_counts, x.seed_counts),
+                        output_counts = sum_tables(acc.output_counts, x.output_counts),
+                        fertilizer_count = acc.fertilizer_count + x.fertilizer_count,
                     }
-                farm_diffs[key][i] = diff
-            end
+
+                    return res
+                end,
+                {
+                    soil_counts = {},
+                    seed_counts = {},
+                    output_counts = {},
+                    fertilizer_count = 0,
+                },
+                fun.iter(farm_diffs[key])
+            )
         end,
         fun.iter(farms)
     ))
 
-    parallel.waitForAll(table.unpack(funcs))
+    pretty.print(pretty.pretty(farm_aggregates))
 end
-
-pretty.print(pretty.pretty(farm_counts.spruce_1))
-pretty.print(pretty.pretty(farm_diffs.spruce_1))
