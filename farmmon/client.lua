750e0dae522cd37ce00e56c80da6254fe0cef11f
Nils Krüger <36828049+n-krueger@users.noreply.github.com>
2023-04-02 21:40:15 +0200

    increase frame width

--- farmmon/client.lua
+++ farmmon/client.lua
@@ -29,7 +29,7 @@ farm_update_thread:start(function()
 end)
 
 local frame_width, frame_height = main_frame:getSize()
-local farm_frame_width = 24
+local farm_frame_width = 25
 local farm_frame_height = 16
 
 local n_cols = math.floor(frame_width / farm_frame_width)
